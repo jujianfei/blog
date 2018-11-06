0dd594f28cdb593be18e9bc4e4ced9872a47bb1d
jujianfei <15732625435@163.com>
2018-11-06 21:06:32 +0800

    initialization

---
+++ src/main/java/cn/jujianfei/blog/Home.java
@@ -0,0 +1,17 @@
+package cn.jujianfei.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Thinkpad
+ * @date 2018/11/6
+ */
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String index(){
+        return "Hello Friend, I am jujianfei.";
+    }
+}
